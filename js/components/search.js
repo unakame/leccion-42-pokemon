aeb4b809d24abd22986178c2ca6a3ce6cc176527
Dana Franco <turquoise.ilg@hotmail.com>
2017-06-16 18:20:12 -0500

    button

--- js/components/search.js
+++ js/components/search.js
@@ -4,7 +4,7 @@ const Search = () => {
   const container = $('<div class="container"></div>');
   const row = $('<div class="row"></div>')
   const search = $('<div class="col-md-7"><div class="input-group"><span class="input-group-addon" id="basic-addon1"><i class="fa fa-search" id="basic-addon1"></i></span><input type="text" class="form-control" placeholder="Pokemon" ></div></div>');
-  const button = $('<div class="col-md-1 col-md-offset-4"><button class="">A-Z</button></div>');
+  const button = $('<div class="col-md-2 col-md-offset-3"><button class=""> A-Z </button></div>');
 
   row.append(search);
   row.append(button);
