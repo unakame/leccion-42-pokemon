97580eeb079db6b370706874090e9b8ed036e0ff
Dana Franco <turquoise.ilg@hotmail.com>
2017-06-17 14:21:31 -0500

    maquetando div

--- js/components/pokemonBox.js
+++ js/components/pokemonBox.js
@@ -1,9 +1,23 @@
 'use strict'
 
 const PokemonBox = () => {
-  const pokemonBox = $('<div class="pokemonBox row"></div>');
-  const pokemonMiniBox = $('<div class="pokemonMiniBox col-md-2"></div>');
+  const pokemonBox      = $('<div class="pokemonBox row"></div>');
+  const pokemonMiniBox  = $('<div class="pokemonMiniBox col-md-2"></div>');
+  const pokemonImg      = $('<img src="http://serebii.net/art/th/39.png">');
+  const pokemonIcons    = $('<div class="icon-box"><a href="#"><img src="icon/pokeball_gray.png" class="icon"></a><a href="#"><img src="icon/valentines-heart.png" class="heart icon"></a><a><img src="icon/data.png" class="icon"></a></div>')
+  const pokemonName     = $('<p>Nombre</p>');
 
   pokemonBox.append(pokemonMiniBox);
+  pokemonMiniBox.append(pokemonImg);
+  pokemonMiniBox.append(pokemonIcons);
+  pokemonMiniBox.append(pokemonName);
+
+
+
+
   return pokemonBox;
 }
+
+/*$('img').click( function() {
+    $(this).css('color', 'red')
+  } );*/
